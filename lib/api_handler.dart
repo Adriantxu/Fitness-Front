9e36202b2b8c8cc2743f285e48f8436d3a25aa57
Oriol Linan <oriol.linan@epitech.eu>
2023-03-23 14:01:02 +0100

    cleaned api handler

--- lib/api_handler.dart
+++ lib/api_handler.dart
@@ -1,29 +1,12 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
 
 final dio = Dio();
-String apiUrl =
-    Platform.isAndroid ? 'http://10.104.66.234:3030' : 'http://localhost:3030';
-
-Future<String> postSignUp() async {
-  final response = await http.post(Uri.parse('${apiUrl}/auth/signup'));
-
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw Exception('Failed to post that');
-  }
-}
+final String apiUrl = Platform.isAndroid ? 'http://10.104.66.234:3030' : 'http://localhost:3030';
 
 Future<String> postLogIn(String email, String password) async {
-  // var toSend = json.encode({'email': email, 'password': password});
-  var toSend = {'email': email, 'password': password};
-  final response = await dio.post('${apiUrl}/auth/login', data: toSend);
-
-  print('Response status code: ${response.statusCode}');
-  print('Response body: ${response.data}');
+  var data = {'email': email, 'password': password};
+  final response = await dio.post('$apiUrl/auth/login', data: data);
 
   if (response.statusCode == 201) {
     return response.data;
